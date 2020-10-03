7f46cc92afc7f08281d418aae532d473d4070b29
yuanjinshuai <arthuryuan15@outlook.com>
2020-10-03 08:38:01 +0800

    add controller

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.repositories.AuthorRepository;
+import com.example.demo.repositories.BookRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/hello")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+
+    @RequestMapping("/test")
+    public String helloWorldTest(){
+        return "hello world";
+    }
+}
